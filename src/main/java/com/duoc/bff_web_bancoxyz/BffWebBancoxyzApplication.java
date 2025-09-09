274ed6b40569e69f5de1239b3d0de20543463f09
LeoOlivaresD <cspraword@gmail.com>
2025-09-09 01:51:51 -0300

    Commit incial de bff web para banco xyz

---
+++ src/main/java/com/duoc/bff_web_bancoxyz/BffWebBancoxyzApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.bff_web_bancoxyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffWebBancoxyzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffWebBancoxyzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duoc/bff_web_bancoxyz/BffWebBancoxyzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.bff_web_bancoxyz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffWebBancoxyzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
