406c8d59c492b5a8cb57d3e554c588d9ef753ba0
LeoOlivaresD <cspraword@gmail.com>
2025-09-09 01:54:05 -0300

    Ajustes iniciales para el proyecto

--- src/main/java/com/duoc/bff_web_bancoxyz/BffWebBancoxyzApplication.java
+++ src/main/java/com/duoc/bff_web_bancoxyz/BffWebBancoxyzApplication.java
@@ -2,8 +2,11 @@ package com.duoc.bff_web_bancoxyz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@EnableFeignClients
 public class BffWebBancoxyzApplication {
 
 	public static void main(String[] args) {
